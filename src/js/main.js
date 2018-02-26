f7345cbc108b72e3554b8a5e24512c5f11741b86
Verthon <krzysiekksordyl@gmail.com>
2018-02-26 13:58:21 +0100

    navigation is now working

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,7 @@ const hamburger = document.getElementById('nav-hamburger');
 const nav = document.getElementById('site-nav');
 
 const toggleHamburger = () => {
-  if(nav.style.display = "none"){nav.style.display = "flex"}
-  else {nav.style.display = "none"}
+  nav.style.display = (nav.style.display = 'none') ? 'flex' : 'none';
 }
 
 hamburger.addEventListener('click', toggleHamburger);
